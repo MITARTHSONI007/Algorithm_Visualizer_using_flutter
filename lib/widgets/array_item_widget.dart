ec898a3f7b001efe5b099905625ebe07ef8eb97a
MITARTHSONI007 <117015020+MITARTHSONI007@users.noreply.github.com>
2024-06-29 01:36:24 +0530

    Add files via upload

---
+++ lib/widgets/array_item_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:sorting_visualizer_flutter/models/array_item.dart';
+
+class ArrayItemWidget extends StatefulWidget {
+  const ArrayItemWidget({
+    Key? key,
+    required this.heightByValue,
+    required this.widthByArraySize,
+    required this.speed,
+  }) : super(key: key);
+
+  final ArrayItem heightByValue;
+  final double widthByArraySize;
+  final int speed;
+
+  @override
+  State<ArrayItemWidget> createState() => _ArrayItemWidgetState();
+}
+
+class _ArrayItemWidgetState extends State<ArrayItemWidget> {
+  // builder  method
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text(
+          widget.heightByValue.value.toString(),
+          style: const TextStyle(
+              color: Color.fromARGB(255, 244, 244, 54), fontWeight: FontWeight.w700, fontSize: 15),
+        ),
+        AnimatedContainer(
+          foregroundDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.black, width: 1),
+          ),
+          curve: Curves.easeInOutQuart,
+          duration: Duration(milliseconds: widget.speed),
+          margin: const EdgeInsets.symmetric(horizontal: 1),
+          width: widget.widthByArraySize,
+          height: MediaQuery.of(context).size.height * 0.01 +
+              widget.heightByValue.value * 0.5,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+          ),
+        ),
+      ],
+    );
+  }
+}
